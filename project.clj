1cd0758d875bb51d9d37db297532f7f71676176a
Christopher Howard <derhowiedesigns@gmail.com>
2017-01-21 17:54:09 -0500

    url fix

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cascade "0.2.0"
   :description "inform collection items with cascading data"
-  :url "http://github.com/derhowie/cascade"
+  :url "http://github.com/fresh2def/cascade"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
